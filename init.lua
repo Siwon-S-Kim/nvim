f340705f0ebb191f3190f89b14bddf1baeaad241
Siwon-S-Kim <siwonworking@gmail.com>
2025-06-20 14:36:43 +0900

    lua dynamic python snippets scavanged init, function def

--- init.lua
+++ init.lua
@@ -1048,6 +1048,6 @@ require('lazy').setup({
 require('luasnip.loaders.from_vscode').load { paths = { '~/.config/nvim/snippets/' } }
 require 'keymap_snippets'
 require 'keymap_general'
-
+require 'custom.plugins.lua_snip.python'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/lua_snip/python.lua
@@ -0,0 +1,2 @@
+require 'custom.plugins.lua_snip.py_dynamic_initializer'
+require 'custom.plugins.lua_snip.py_function_def'
