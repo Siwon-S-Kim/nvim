7ecfd365183e94475d3ed38c488a9bf35d90cf42
Siwon-S-Kim <siwonworking@gmail.com>
2026-03-13 10:36:03 +0900

    temp iron commit

--- init.lua
+++ init.lua
@@ -997,6 +997,10 @@ require('lazy').setup({
   {
     'AndrewRadev/linediff.vim',
   },
+  {
+    'Vigemus/iron.nvim',
+    config = true,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
